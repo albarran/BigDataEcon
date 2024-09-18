23c08834fc927648e891799402209d1f272f93aa
albarran <albarran@ua.es>
2024-09-18 08:25:18 +0200

    Tema0Ej1

---
+++ docs/Tema00ej1_12345678.R
@@ -0,0 +1,84 @@
+############################################################
+## Tema 00. Ejercicio 1
+## NOMBRE: (reemplaza esto con tu Nombre y Apellido(s))
+## DNI:    (reemplaza esto con tu DNI, o similar)
+############################################################
+
+
+## escribe tus propios pasos aqui
+## no olvides hacer comentarios si lo consideras necesario
+
+
+###############################################################################
+
+###---------- Apartado 1) ----------------------------------------------------
+
+# Ventas totales por día
+Ventas_totales_diarias
+
+# Total de ventas semanales
+Ventas_totales_semana
+
+# Ventas medias de la semana en A
+Ventas_media_A
+
+###############################################################################
+
+###---------- Apartado 2.a) ----------------------------------------------------
+
+datos2
+
+###---------- Apartado 2.b) ----------------------------------------------------
+
+# Calcular total ventas
+datos2$Total
+
+###---------- Apartado 2.c) ----------------------------------------------------
+
+# media del valor de las ventas internacionales
+media_internac
+
+# media del valor de las ventas nacionales pero solo para los productos X, Y y R.
+media_nacional_XYR
+
+
+###---------- Apartado 2.c) ----------------------------------------------------
+
+# Data Frame de precios
+precios
+
+# Añadir precios a datos2
+datos2 
+
+# Calcular número de compradores
+datos2$compradoresN 
+datos2$compradoresI
+
+###############################################################################
+
+###---------- Apartado 3.a) ----------------------------------------------------
+
+# Mostrar la estructura del conjunto de datos
+str(mtcars)
+
+# Mostrar las primeras filas (observaciones)
+head(mtcars)
+
+
+Datsun710_CV
+
+Valiant_vector
+
+###---------- Apartado 3.b) ----------------------------------------------------
+
+cilindros
+
+cambio_vector
+
+###---------- Apartado 3.c) ----------------------------------------------------
+
+
+media_consumo_autom 
+
+
+media_consumo_autom_cyl4
